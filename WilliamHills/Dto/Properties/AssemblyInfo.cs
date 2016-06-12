2ffd46d1341272e57bcb25044e7f4f472b9602f9
Dell <Dell@MEHDI-DELL>
2016-06-12 13:49:47 +1000

    Updated Project

--- WilliamHills/Dto/Properties/AssemblyInfo.cs
+++ WilliamHills/Dto/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dtos")]
+[assembly: AssemblyTitle("Dto")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dtos")]
+[assembly: AssemblyProduct("Dto")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad318947-686d-4396-ab06-934285cd4684")]
+[assembly: Guid("909ffa54-c1a8-4200-aa08-5360966cfc47")]
 
 // Version information for an assembly consists of the following four values:
 //
